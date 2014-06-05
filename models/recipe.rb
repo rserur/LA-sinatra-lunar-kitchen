2f036d4ef6739a5a116e10749821383ccde17d14
Rachael Serur <rachaelse@gmail.com>
2014-06-05 13:27:46 -0400

    DB entries successfully converted into objects and collected under Recipe.all-- successfully outputs to /recipes.

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,36 @@
+require 'pry'
+
+def get_db
+  connection = PG.connect(dbname: 'recipe_box')
+  results = connection.exec('SELECT * FROM recipes ORDER BY name ASC')
+  connection.close
+  results.to_a
+end
+
 class Recipe
+  def initialize(recipe)
+    @id = recipe["id"]
+    @name = recipe["name"]
+    @instructions = recipe["instructions"]
+    @description= recipe["description"]
+  end
+
+  def self.all
+    ObjectSpace.each_object(self).to_a
+  end
+
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+end
+
+all_recipes = get_db
+
+all_recipes.each do |db_entry|
+  recipe = Recipe.new(db_entry)
 end
